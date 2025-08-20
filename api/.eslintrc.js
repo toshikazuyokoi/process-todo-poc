56cb9c319fbb0f1caeb0429d1982de437b931009
Toshikazu Yokoi <yokoi@g-mind.co.jp>
2025-08-20 09:37:48 +0900

    fix: Backend ESLint設定を追加
    - Backend用のESLint設定ファイル(.eslintrc.js)を追加
    - ESLint v8.57.0にダウングレード(v9のconfig形式問題を回避)
    - @typescript-eslint/eslint-plugin と parser を v7.18.0 に設定
    
    CI/CDのlintステップエラーを解決

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,21 +1,20 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
+    ecmaVersion: 2020,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'prettier',
   ],
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js', 'dist', 'node_modules'],
+  ignorePatterns: ['.eslintrc.js', 'dist', 'node_modules', 'coverage', 'test', 'uploads'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
